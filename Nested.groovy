a6fa3ce2f8c989ab2338bb47444ea336affe7f09
Blair Learn <blairlearn@users.noreply.github.com>
2018-05-09 18:18:10 -0400

    run

--- Nested.groovy
+++ Nested.groovy
@@ -2,10 +2,10 @@ import org.codehaus.groovy.runtime.InvokerHelper
 
 class Nested extends Script {
     def run() {
-        def NAME_LENGTH = JOB_BASE_NAME.length()
-        def FOLDER_PATH = JOB_NAME[0..((JOB_NAME.length() - NAME_LENGTH) - 2)]
+        //def NAME_LENGTH = JOB_BASE_NAME.length()
+        //def FOLDER_PATH = JOB_NAME[0..((JOB_NAME.length() - NAME_LENGTH) - 2)]
 
-        job("${FOLDER_PATH}/Here there be dragons") {
+        job("/wcms-cde/_lower/Here there be dragons") {
 
         }
     }
