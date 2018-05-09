9b97020283eec74681365384a9bc12ddb7b70db7
Blair Learn <blairlearn@users.noreply.github.com>
2018-05-09 18:06:51 -0400

    class

---
+++ Nested.groovy
@@ -0,0 +1,15 @@
+import org.codehaus.groovy.runtime.InvokerHelper
+
+class Nested extends Script {
+def doStuff() {
+    def NAME_LENGTH = JOB_BASE_NAME.length()
+    def FOLDER_PATH = JOB_NAME[0..((JOB_NAME.length() - NAME_LENGTH) - 2)]
+
+    job("${FOLDER_PATH}/Here there be dragons") {
+
+    }
+}
+
+//return [
+//    doStuff: this.&doStuff
+//]
