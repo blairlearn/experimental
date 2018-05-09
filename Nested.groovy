95a240d942a437bed2ab15fd2a9e5bcedd8247cf
Blair Learn <blairlearn@users.noreply.github.com>
2018-05-09 18:10:16 -0400

    balanced brackets

--- Nested.groovy
+++ Nested.groovy
@@ -1,12 +1,13 @@
 import org.codehaus.groovy.runtime.InvokerHelper
 
 class Nested extends Script {
-def doStuff() {
-    def NAME_LENGTH = JOB_BASE_NAME.length()
-    def FOLDER_PATH = JOB_NAME[0..((JOB_NAME.length() - NAME_LENGTH) - 2)]
+    def doStuff() {
+        def NAME_LENGTH = JOB_BASE_NAME.length()
+        def FOLDER_PATH = JOB_NAME[0..((JOB_NAME.length() - NAME_LENGTH) - 2)]
 
-    job("${FOLDER_PATH}/Here there be dragons") {
+        job("${FOLDER_PATH}/Here there be dragons") {
 
+        }
     }
-}
 
+}
