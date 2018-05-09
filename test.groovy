2b7f1a4eb65cbda758c930b72e0cbba2d7f7d365
Blair Learn <blairlearn@users.noreply.github.com>
2018-05-09 18:01:34 -0400

    nested

--- test.groovy
+++ test.groovy
@@ -1,5 +1,7 @@
-println "Start"
-def inner  = evaluate readFileFromWorkspace("inner.groovy")
-println "Post-eval"
-inner.doStuff()
-println "Fine"
+//println "Start"
+//def inner  = evaluate readFileFromWorkspace("inner.groovy")
+//println "Post-eval"
+//inner.doStuff()
+//println "Fine"
+
+import Nested
